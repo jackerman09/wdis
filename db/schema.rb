712a1313a9e58661a69e1b2d0c42ace06063b748
jackerman09 <jackerman09@gmail.com>
2013-09-26 17:43:12 -0400

    add full_name to player and early search form to home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921215624) do
+ActiveRecord::Schema.define(version: 20130926214223) do
 
   create_table "matchplays", force: true do |t|
     t.integer  "player_id"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20130921215624) do
     t.datetime "updated_at"
     t.integer  "team_id"
     t.string   "image_name"
+    t.string   "full_name"
   end
 
   create_table "relationships", force: true do |t|

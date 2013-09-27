675a63f09053e5407ef55194aa6e1e87cdab0c75
jackerman09 <jackerman09@gmail.com>
2013-09-26 23:36:00 -0400

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+teams = Team.create(name: 'Jets')
+players = Player.create(
+	[last_name: 'Gore', first_name: 'Frank', team_id: 1],
+	[last_name: 'Peterson', first_name: 'Adrian', team_id: 1],
+	)
+matchups = Matchup.create(player_1: 1, player_2: 2)
+users = User.create(name: 'Jason Ackerman', email: 'jackerman09@gmail.com', password: 'railstutorial', password_confirmation: 'railstutorial', admin: true)

57704c487be5aceb0932077df916596c7cf549c5
jackerman09 <jackerman09@gmail.com>
2013-09-30 17:11:59 -0400

    pre-push to heroku

--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -49,10 +49,15 @@ class Matchup < ActiveRecord::Base
 	validates :player_1, presence: true
 	validates :player_2, presence: true
 
-	before_create :set_matchup_pts_to_0
+	before_create		:set_matchup_pts_to_0
+	before_save			:update_total_votes
 
 	has_many :matchplays
 	has_many :players, through: :matchplays
+
+	def current_week
+		1
+	end
 
 	private
 		def set_matchup_pts_to_0
@@ -90,5 +95,11 @@ class Matchup < ActiveRecord::Base
 			self.pts_player_2_week_15 = 0
 			self.pts_player_2_week_16 = 0
 			self.pts_player_2_week_17 = 0
+			self.total_votes = 0
+		end
+
+		def update_total_votes
+			current_week = self.current_week
+			self.total_votes = send("pts_player_1_week_#{current_week}") + send("pts_player_2_week_#{current_week}")
 		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+  before_create { self.num_credits = 0 }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
